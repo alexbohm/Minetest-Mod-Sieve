04912f393b464dc23a73d0103a65e8cdc96079d4
Alex Bohm <aabohm7@gmail.com>
2016-03-12 21:09:01 -0600

    Added files via upload

--- init.lua
+++ init.lua
@@ -1,6 +1,80 @@
 minetest.log("action","Sieve: Starting Load")
 --Created by Alex Bohm
+--[[
+Notes:
 
+MetaData Ref
+"burn_t" = total seconds of burn time on place and take
+"mat_n" = total number of material to process
+
+Auto Sieve is not functional yet (almost there).
+]]
+function con_mat(pos, stack)
+    local meta = minetest.get_meta(pos)
+    local inv = meta:get_inventory()
+    if inv:contains_item("mat", stack) then
+        inv:remove_item("mat", ItemStack(stack:get_name().." 1"))
+        inv:add_item("out", ItemStack(material(stack:get_name())))
+    end
+end
+function con_fuel(pos, stack)
+    local meta = minetest.get_meta(pos)
+    local inv = meta:get_inventory()
+    if inv:contains_item("fuel", stack) then
+        inv:remove_item("fuel", ItemStack(stack:get_name().." 1"))
+    end
+end
+
+function accept(itemname)
+    if itemname == 'default:sand' or itemname == 'default:gravel' or itemname == 'default:desert_sand' or itemname == 'default:dirt' then
+        return true
+    else
+        return false
+    end
+    --[[
+    Acceptable Items:
+    "default:sand"
+    "default:gravel"
+    "default:desert_sand"
+    "default:dirt"
+    ]]
+end
+
+function material(itemname)
+    local chance = math.random(40)
+        if accept(itemname) then
+            if chance <= 5 then
+                --stick
+                r_str = 'default:stick 1'
+            elseif chance <= 10 and chance < 12 then
+                --coal
+                r_str = 'default:coal_lump 1'
+            elseif chance <= 12 and chance < 14 then
+                --gold
+                r_str = 'default:gold_lump 1'
+            elseif chance <= 14 and chance < 16 then
+                --copper
+                r_str = 'default:copper_lump 1'
+            elseif chance <= 16 and chance < 18 then
+                --iron
+                r_str = 'default:iron_lump 1'
+            elseif chance <= 18 and chance < 19 then 
+                --mese fragment
+                r_str = 'default:mese_crystal_fragment 1'
+            elseif chance <= 19 and chance < 20 then
+                --mese crystal
+                r_str = 'default:mese_crystal 1'
+            elseif chance == 20 then
+                --mese gem
+                r_str = 'default:diamond 1'
+            else
+                return nil
+            end
+            return r_str
+        else
+            return nil
+        end
+end
 --Cloth Fiber
 minetest.register_craftitem("sieve:cloth_fiber", {
 	description = "Cloth Fiber",
@@ -50,9 +124,9 @@ minetest.register_craftitem("sieve:shaker_frame", {
 minetest.register_craft({
     output = "sieve:shaker_frame 1",
     recipe = {
-        {"", "sieve:shaker_motor",""},
+        {"default:steel_ingot", "sieve:shaker_motor","default:steel_ingot"},
         {"sieve:shaker_motor", "default:furnace","sieve:shaker_motor"},
-        {"", "sieve:shaker_motor",""},
+        {"default:steel_ingot", "sieve:shaker_motor","default:steel_ingot"},
     }
 })
 minetest.log("action","Sieve: Shaker Frame Loaded")
@@ -86,123 +160,10 @@ minetest.register_node("sieve:hand_sieve", {
         },
     on_rightclick = function(pos, node, player, itemstack, pointed_thing)
         --minetest.chat_send_all(itemstack:to_string())
-        if itemstack:get_name() == 'default:sand' then
+        if accept(itemstack:get_name()) then
             itemstack:take_item()
-            local chance = math.random(40)
-            if chance <= 5 then
-                --stick
-                minetest.item_drop(ItemStack('default:stick 1'), player, pos)
-            elseif chance <= 10 then
-                --coal
-                minetest.item_drop(ItemStack('default:coal_lump 1'), player, pos)
-            elseif chance <= 12 then
-                --gold
-                minetest.item_drop(ItemStack('default:gold_lump 1'), player, pos)
-            elseif chance <= 14 then
-                --copper
-                minetest.item_drop(ItemStack('default:copper_lump 1'), player, pos)
-            elseif chance <= 16 then
-                --iron
-                minetest.item_drop(ItemStack('default:iron_lump 1'), player, pos)
-            elseif chance <= 18 then 
-                --mese fragment
-                minetest.item_drop(ItemStack('default:mese_crystal_fragment 1'), player, pos)
-            elseif chance == 19 then
-                --mese crystal
-                minetest.item_drop(ItemStack('default:mese_crystal 1'), player, pos)
-            elseif chance == 20 then
-                --mese gem
-                minetest.item_drop(ItemStack('default:diamond 1'), player, pos)
-            end
-        elseif itemstack:get_name() == 'default:gravel' then
-            itemstack:take_item()
-            local chance = math.random(40)
-            if chance <= 5 then
-                --stick
-                minetest.item_drop(ItemStack('default:stick 1'), player, pos)
-            elseif chance <= 10 then
-                --coal
-                minetest.item_drop(ItemStack('default:coal_lump 1'), player, pos)
-            elseif chance <= 12 then
-                --gold
-                minetest.item_drop(ItemStack('default:gold_lump 1'), player, pos)
-            elseif chance <= 14 then
-                --copper
-                minetest.item_drop(ItemStack('default:copper_lump 1'), player, pos)
-            elseif chance <= 16 then
-                --iron
-                minetest.item_drop(ItemStack('default:iron_lump 1'), player, pos)
-            elseif chance <= 18 then 
-                --mese fragment
-                minetest.item_drop(ItemStack('default:mese_crystal_fragment 1'), player, pos)
-            elseif chance == 19 then
-                --mese crystal
-                minetest.item_drop(ItemStack('default:mese_crystal 1'), player, pos)
-            elseif chance == 20 then
-                --mese gem
-                minetest.item_drop(ItemStack('default:diamond 1'), player, pos)
-            end
-        elseif itemstack:get_name() == 'default:desert_sand' then
-            itemstack:take_item()
-            local chance = math.random(40)
-            if chance <= 5 then
-                --stick
-                minetest.item_drop(ItemStack('default:stick 1'), player, pos)
-            elseif chance <= 10 then
-                --coal
-                minetest.item_drop(ItemStack('default:coal_lump 1'), player, pos)
-            elseif chance <= 12 then
-                --gold
-                minetest.item_drop(ItemStack('default:gold_lump 1'), player, pos)
-            elseif chance <= 14 then
-                --copper
-                minetest.item_drop(ItemStack('default:copper_lump 1'), player, pos)
-            elseif chance <= 16 then
-                --iron
-                minetest.item_drop(ItemStack('default:iron_lump 1'), player, pos)
-            elseif chance <= 18 then 
-                --mese fragment
-                minetest.item_drop(ItemStack('default:mese_crystal_fragment 1'), player, pos)
-            elseif chance == 19 then
-                --mese crystal
-                minetest.item_drop(ItemStack('default:mese_crystal 1'), player, pos)
-            elseif chance == 20 then
-                --mese gem
-                minetest.item_drop(ItemStack('default:diamond 1'), player, pos)
-            end
-        elseif itemstack:get_name() == 'default:dirt' or itemstack:get_name() == 'default:dirt_with_grass' then
-            itemstack:take_item()
-            if itemstack:get_name() == 'default:dirt_with_grass' then
-                minetest.item_drop(ItemStack('default:grass_4 1'), player, pos)
-            end
-            local chance = math.random(40)
-            if chance <= 5 then
-                --stick
-                minetest.item_drop(ItemStack('default:stick 1'), player, pos)
-            elseif chance <= 10 then
-                --coal
-                minetest.item_drop(ItemStack('default:coal_lump 1'), player, pos)
-            elseif chance <= 12 then
-                --gold
-                minetest.item_drop(ItemStack('default:gold_lump 1'), player, pos)
-            elseif chance <= 14 then
-                --copper
-                minetest.item_drop(ItemStack('default:copper_lump 1'), player, pos)
-            elseif chance <= 16 then
-                --iron
-                minetest.item_drop(ItemStack('default:iron_lump 1'), player, pos)
-            elseif chance <= 18 then 
-                --mese fragment
-                minetest.item_drop(ItemStack('default:mese_crystal_fragment 1'), player, pos)
-            elseif chance == 19 then
-                --mese crystal
-                minetest.item_drop(ItemStack('default:mese_crystal 1'), player, pos)
-            elseif chance == 20 then
-                --mese gem
-                minetest.item_drop(ItemStack('default:diamond 1'), player, pos)
-            end
+            minetest.item_drop(ItemStack(material(itemstack:get_name())), player, pos)
         end
-        return itemstack
     end
     
 })
@@ -290,6 +251,70 @@ minetest.register_node("sieve:auto_sieve", {
                 {0.5, -0.5, -0.5, 0.4375, 0.5, -0.4375}, --leg
                 },
         },
+    on_construct = function(pos)
+        local meta = minetest.get_meta(pos)
+        meta:set_string("formspec",
+        "size[8,9;]"..
+        "list[context;mat;0,0;1,1;]"..
+        "list[context;fuel;0,3;1,1;]"..
+        "list[context;out;3,0;5,4;]"..
+        "image[0,1;2,2;sieve_auto_sieve_side.png]"..
+        "list[current_player;main;0,5;8,4;]")
+        meta:set_int("burn_t", 0)
+        local inv = meta:get_inventory()
+        inv:set_size("mat", 1*1)
+        inv:set_size("fuel", 1*1)
+        inv:set_size("out", 5*4) 
+    end,
+    allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
+        return 0
+    end,
+    allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+        if listname == 'fuel' then
+            if minetest.get_craft_result({method="fuel",width=1,items={stack}}).time > 0 then
+                return stack:get_count()
+            else
+                return 0
+            end
+        elseif listname == 'mat' then
+            if accept(stack:get_name()) then
+                return stack:get_count()
+            else
+                return 0
+            end
+        elseif listname == 'out' then
+            return 0
+        end
+    end,
+    on_metadata_inventory_put = function(pos, listname, index, stack, player)
+        local meta = minetest.get_meta(pos)
+        local inv = meta:get_inventory()
+        if listname == "fuel" then
+            meta:set_int("burn_t", inv:get_stack("fuel", 1):get_count()*minetest.get_craft_result({method="fuel",width=1,items={stack}}).time)
+        elseif listname == "mat" then
+            meta:set_int("mat_n", inv:get_stack("mat_n", 1):get_count())
+            con_mat(pos, stack)
+        end
+    end,
+    on_metadata_inventory_take = function(pos, listname, index, stack, player)
+        local meta = minetest.get_meta(pos)
+        local inv = meta:get_inventory()
+        if listname == "fuel" then
+            meta:set_int("burn_t", inv:get_stack("fuel", 1):get_count()*minetest.get_craft_result({method="fuel",width=1,items={stack}}).time)
+        elseif listname == "mat" then
+            meta:set_int("mat_n", inv:get_stack("mat_n", 1):get_count())
+        end
+    end,
+    on_destruct = function(pos)
+        local meta = minetest.get_meta(pos)
+        local inv = meta:get_inventory()
+    for i, item in ipairs(inv:get_list("out")) do
+        minetest.add_item(pos, item)
+    end
+    minetest.add_item(pos, inv:get_stack("mat", 1))
+    minetest.add_item(pos, inv:get_stack("fuel", 1))
+    end,
+
 })
 minetest.register_craft({
     output = "sieve:auto_sieve",
